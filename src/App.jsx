9538c2564c2accb7248feb9a2a0981267fea67ec
lucasdsaints <lucas.assis.santos96@gmail.com>
2020-10-16 21:42:45 -0300

    chore: configura os slices a serem utilizados na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { Counter } from './features/Counter';
-import './App.css';
-import Sidebar from './Sidebar/Sidebar';
-import Chat from './Chat/Chat';
+import React from 'react'
+import './App.css'
+import Sidebar from './Sidebar/Sidebar'
+import Chat from './Chat/Chat'
+import { useSelector } from 'react-redux'
+import { selectUser } from './store/userSlice'
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ): (
+        <h2>You need to login</h2>
+      )}
     </div>
   );
 }
